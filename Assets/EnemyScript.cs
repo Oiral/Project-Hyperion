2e101126b987b366fe8f05a15199bc93c5a69311
Unknown <hamish@pageland.co.nz>
2018-07-24 13:47:29 +1200

    Added in the unity project and a basic card damage and click system

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyScript : MonoBehaviour {
+
+    public int health;
+
+    public Text healthText;
+
+    public void ChangeHealth(int healthToChange)
+    {
+        health += healthToChange;
+        if (health <= 0)
+        {
+            Debug.Log("Im dead");
+            health = 0;
+        }
+        healthText.text = health + "/10";
+    }
+}
