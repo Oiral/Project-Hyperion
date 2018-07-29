68a99fad319559a8e1e1758cef284ddb9a0399a1
Unknown <bjcox93@gmail.com>
2018-07-30 11:47:44 +1200

    GB, Music and SFX
    GB
    Music
    Footsteps

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    public static int previousSceneIndex;
+
+	// Use this for initialization
+	void Awake () {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else if (instance != this)
+        { Destroy(gameObject); }
+        DontDestroyOnLoad(gameObject);
+        previousSceneIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+
+
+    public float speed;
+    public float walkSoundDelay;
+    private Rigidbody rb;
+    private bool walking;
+    public AudioClip walkSound;
+    private AudioSource source;
+    private float volLowRange = 0.5f;
+    private float volHighRange = 1.0f;
+    public bool walkingBool;
+    bool soundBool = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
+        walkingBool = false;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            walkingBool = true;
+            Debug.Log("wB = true");
+        }
+
+        if (walkingBool && soundBool)
+        {
+            float vol = Random.Range(volLowRange, volHighRange);
+            soundBool = false;
+            source.PlayOneShot(walkSound, vol);
+            Debug.Log("sfx_playing");
+            StartCoroutine(ResetSoundBool());
+        }
+    }
+    IEnumerator ResetSoundBool()
+    {
+        yield return new WaitForSeconds(walkSoundDelay);
+        soundBool = true;
+        print("WB = false");
+    }
+}
