5ac8aac0fb5255c4ec180e9ef87a9010f9beb531
Hamish <hamish@pageland.co.nz>
2018-08-02 22:32:22 +1200

    Basic Hand setup
    Adding in some visual hand stuff so it seems like you actually holding the cards

---
+++ Assets/HandScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandScript : MonoBehaviour {
+
+    public List<GameObject> itemsInHand;
+
+    private void Start()
+    {
+        for (int i = 0; i < itemsInHand.Count; i++)
+        {
+            //x goes between -1 and 1
+            float x = (float)i / ((float)itemsInHand.Count - 1);
+            x = (x - 0.5f) * 2;
+
+            itemsInHand[i].transform.position = transform.position;
+            itemsInHand[i].transform.position += new Vector3(x * 50, 0);
+            itemsInHand[i].transform.rotation = Quaternion.Euler(new Vector3(0, 0, x * -40));
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Start();
+        }
+    }
+}
