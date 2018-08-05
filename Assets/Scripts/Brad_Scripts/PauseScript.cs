a8ea085ee9fbeea96bfa398f2e33324e1879eab7
Unknown <bjcox93@gmail.com>
2018-08-06 10:22:37 +1200

    Menu update_1
    Script and pause menu update

---
+++ Assets/Scripts/Brad_Scripts/PauseScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour {
+
+    public void UnPauseGame()
+    {
+        SceneManager.LoadScene(GameManager.previousSceneIndex);
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
