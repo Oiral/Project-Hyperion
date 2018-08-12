916b809c30133f4f84b0b1317bcb274022e1a4f1
Unknown <smoatwork@gmail.com>
2018-08-13 10:01:46 +1200

    Added Scripts
    Added player info + first selectable script

---
+++ Assets/Scripts/Info Manager Scripts/PlayerInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+	public Foe foeType;
+	public Deck deck;
+	public int startHealth;
+
+	private void Start()
+	{
+		startHealth = Mathf.Abs(startHealth);
+	}
+}
