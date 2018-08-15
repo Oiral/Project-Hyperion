8bf67af112b3d797654623a20e80dd0c72bbba6f
Unknown <bjcox93@gmail.com>
2018-08-16 09:24:49 +1200

    Character update, Music

--- Assets/Scripts/Brad_Scripts/Enemie_Script.cs
+++ Assets/Scripts/Brad_Scripts/Enemie_Script.cs
@@ -20,7 +20,7 @@ public class Enemie_Script : MonoBehaviour {
         if (other.gameObject.tag == "Player")
         {
             GameManager.previousSceneIndex = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(5);
+            SceneManager.LoadScene(4);
         }
             
     }
