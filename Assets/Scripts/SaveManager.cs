045b938df4f5e8f5d7f5138803a2d3c92f147432
Unknown <smoatwork@gmail.com>
2018-08-25 16:44:12 +1200

    Working on save game manager

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/* 
+ * The save file works by breaking itself up into sections with 
+ * 
+ *					++++
+ * 
+ * If you want to add to the save file, create a section like this.
+ * 
+ * Each section is split between a header string and data
+ * The section should look like: 
+ * 
+ *					"[Header]|||[Data]"
+ *					
+ * For example:
+ * 
+ *					Deck|||
+ *					++++
+ *					Enemies_defeated|||1010
+ * 
+ * Happy editing!
+ */
+
+public class SaveManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
